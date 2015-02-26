df6e814f83a7f7fcd4de9b3e7e21dfee5dca742e
Kip Brandt <kipbrandt@gmail.com>
2015-02-26 22:30:45 +0000

    Chapter 011 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.decorators import login_required
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from rango.models import Category, Page
 
+from datetime import datetime
+
 def index(request):
 
     context_dict ={}
@@ -18,13 +20,46 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    # Render the response and send it back!
-    return render(request, 'rango/index.html', context_dict)
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            visits = visits + 1
+            # ...and update the last visit cookie, too.
+            reset_last_visit_time = True
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+    context_dict['visits'] = visits
+
+
+    response = render(request,'rango/index.html', context_dict)
+
+    return response
+
 
 
 def about(request):
     
         context_dict = {}
+
+        if request.session.get('visits'):
+            count = request.session.get('visits')
+        else:
+            count = 0
+        context_dict['visits'] = count
+        
         return render(request, 'rango/about.html', context_dict)
     
 
