45bca2148db77a927ac76ea29ca34311e42f6b89
Kip Brandt <kipbrandt@gmail.com>
2015-03-09 20:00:07 +0000

    Chapter 16&17 - completed

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -7,6 +7,8 @@ urlpatterns = patterns('',
         url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'),
         url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
         url(r'^restricted/', views.restricted, name='restricted'),
-        url(r'^search/', views.search, name='search'),
+        url(r'^profile/', views.profile, name='profile'),
+        url(r'^add_profile/', views.register_profile, name='add_profile'),
+        url(r'^goto/$', views.track_url, name='goto'),
         url(r'^add_category/$', views.add_category, name='add_category')
         )
